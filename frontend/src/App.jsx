c62eb20eba812484f96bf563f48ce143e0c96dcc
Igor Martins <igormartins16@gmail.com>
2025-04-04 14:42:05 -0400

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router";
+import { Login } from "./pages/auth/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
