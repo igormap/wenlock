31b9bccdef7240cdc4041cfebb261298061814db
Igor Martins <igormartins16@gmail.com>
2025-04-08 08:18:06 -0400

    build: add docker compose

--- frontend/src/layouts/auth/index.tsx
+++ frontend/src/layouts/auth/index.tsx
@@ -1,5 +1,3 @@
-// import SvgLock from "@/assets/img/illustrations/LockPassword.svg?react";
-// import SvgLogo from "@/assets/img/logos/WenLockLogoWhite.svg?react";
 import WenlockLogo from "@/assets/img/logos/WenlockLogoWhite";
 import { Outlet, useLocation } from "react-router";
 
@@ -10,10 +8,6 @@ const LayoutAuth = () => {
     <div className="bg-slate-900 w-full grid grid-cols-2 h-screen">
       <div className="flex justify-center items-center">
         <WenlockLogo className="w-1/2" />
-        {/* {location.pathname === "/password-recovery" ? (
-        ) : (
-          <SvgLogo className="w-1/2" />
-        )} */}
       </div>
       <div className="p-10">
         <Outlet />
